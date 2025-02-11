98c35f8f255e04f553b2d6b0368a58c2f0e72b53
Lilit <lilithhakobyann@gmail.com>
2025-02-11 14:39:47 +0400

    Kargin button

--- app/src/main/java/com/example/ninja/Kargin_quizzes.java
+++ app/src/main/java/com/example/ninja/Kargin_quizzes.java
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment;
 public class Kargin_quizzes extends Fragment {
 
     public Kargin_quizzes() {
-        // Required empty public constructor
+        
     }
 
     @Nullable
--- app/src/main/java/com/example/ninja/MainActivity.java
+++ app/src/main/java/com/example/ninja/MainActivity.java
@@ -1,24 +1,77 @@
 package com.example.ninja;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button karginButton, vitaminButton;
+
+    @SuppressLint({"ResourceType", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.top_section), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        karginButton = findViewById(R.id.kargin_card_button);
+        vitaminButton = findViewById(R.id.vitamin_card_button);
+
+        karginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFragment(new Kargin_quizzes());
+            }
+        });
+
     }
+
+    private void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+        findViewById(R.id.top_section).setVisibility(View.GONE);
+        findViewById(R.id.quizzes_title).setVisibility(View.GONE);
+        findViewById(R.id.kargin_card).setVisibility(View.GONE);
+        findViewById(R.id.vitamin_card).setVisibility(View.GONE);
+
+        findViewById(R.id.fragment_container).setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            getSupportFragmentManager().popBackStack();
+            findViewById(R.id.top_section).setVisibility(View.VISIBLE);
+            findViewById(R.id.quizzes_title).setVisibility(View.VISIBLE);
+            findViewById(R.id.kargin_card).setVisibility(View.VISIBLE);
+            findViewById(R.id.vitamin_card).setVisibility(View.VISIBLE);
+            findViewById(R.id.fragment_container).setVisibility(View.GONE);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
+
 }
