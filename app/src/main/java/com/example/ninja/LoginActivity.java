cc49f56950170703bdede186da23e08b5188d301
elaleksanyan <elizaa.aleksanyan@gmail.com>
2025-02-11 21:30:58 +0400

    Login xml

---
+++ app/src/main/java/com/example/ninja/LoginActivity.java
@@ -0,0 +1,56 @@
+package com.example.ninja;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.layout.activity_main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        EditText email = findViewById(R.id.textEmail);
+        EditText pass = findViewById(R.id.textPass);
+        ImageView showPass = findViewById(R.id.showPass);
+        Button login = findViewById(R.id.btnLogin);
+        Button logGoogle = findViewById(R.id.btnGoogle);
+        TextView textView = findViewById(R.id.regNow);
+        TextView forgotPass = findViewById(R.id.forgotPass);
+
+
+
+        String fullText = "Don't have an account? Register Now";
+        SpannableString spannable = new SpannableString(fullText);
+
+        spannable.setSpan(new ForegroundColorSpan(Color.parseColor("#4A5DF3")),
+                fullText.indexOf("Register Now"),
+                fullText.length(),
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textView.setText(spannable);
+
+
+
+    }
+}
